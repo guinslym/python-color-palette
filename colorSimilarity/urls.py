a086b7cfd6892ee3b43a88ce4bab3d6db26771f8
Guinsly Mondésir <guinslym@gmail.com>
2017-11-13 12:31:08 -0500

    moving files

--- colorSimilarity/urls.py
+++ colorSimilarity/urls.py
@@ -21,5 +21,5 @@ from applications.brandcolors.views import HomeRedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include("brandcolors.urls")),
+    url(r'^', include("applications.brandcolors.urls", namespace='brandcolors')),
 ]
