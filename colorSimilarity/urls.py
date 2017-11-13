f3199b2ff6a8c67a0f8dc9e6091b6dda2f0a1fa3
Guinsly Mondésir <guinslym@gmail.com>
2017-11-13 12:56:08 -0500

    end of the day

--- colorSimilarity/urls.py
+++ colorSimilarity/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url, include
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 from applications.brandcolors.views import HomeRedirectView
 
@@ -8,3 +13,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include("applications.brandcolors.urls", namespace='brandcolors')),
 ]
+
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
